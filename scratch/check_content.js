2f8a766b199799e2b8928d555bf12161834479e6
datnqbv <np21062004@gmail.com>
2026-09-24 17:06:37 +0700

    delete_img

--- scratch/check_content.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('modules/Lop_10/SH10_B07_M02/SH10_B07_M02.html', 'utf8');
-
-const s4Idx = html.indexOf('id="stage-4"');
-console.log('--- Stage 4 Image and Buttons ---');
-console.log(html.substring(s4Idx, s4Idx + 2200));
